ad001e3c377028efc65371ed8c5c36718afb44b3
Scott M Parrish <anithri@gmail.com>
2020-01-03 18:47:15 -0700

    post filter control

--- src/components/posts/Home.js
+++ src/components/posts/Home.js
@@ -1,10 +1,45 @@
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
 import SummaryList from './SummaryList'
 
+const SelectButton = ({ name, className, doClick }) => {
+  return (
+    <li className={className} onClick={doClick}>
+      <div>
+        <header>{name}</header>
+      </div>
+    </li>
+  )
+}
+
 const PostHome = ({ children, className, page, posts }) => {
-  const [openSection, changeSection] = useState(-1)
-  // const recentPosts = posts.slice(0, 3)
+  const [selected, filterBy] = useState({ year: 'All', month: 'All' })
+  const updateYear = year => {
+    if (year == 'All' && selected.year !== 'All') {
+      filterBy({ year: 'All', month: 'All' })
+    } else if (selected.year !== year) {
+      filterBy({ ...selected, year })
+    }
+  }
+  const updateMonth = (month) => {
+    console.log('updateMonth', month, selected)
+    filterBy({ ...selected, month })
+    console.log('  result: ', selected)
+  }
+  const allYears = ['All', '2020', '2019', '2018']
+  const allMonths = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
   // const postsByYear = {}
   // const postsByYearMonth = {}
   //
@@ -27,107 +62,63 @@ const PostHome = ({ children, className, page, posts }) => {
   //   .map(k => {console.log('listByYear', k); return k})
   //   .sort((a, b) => b - a)
   //   .map((year, idx) =>
-  //     <SummaryList doClick={(e) => changeSection(idx + 1)} isOpen={idx === openSection} posts={postsByYear[year]} title={year} key={`year-${year}`} />,
-  //   )
+  //     <SummaryList doClick={(e) => changeSection(idx + 1)} isOpen={idx === openSection}
+  // posts={postsByYear[year]} title={year} key={`year-${year}`} />, )
+
+  const years = allYears.map(year => {
+    const isSelected = year === selected.year
+    const className = ['yearBtn', isSelected && 'selected']
+      .filter(c => c)
+      .join(' ')
+    return (
+      <SelectButton
+        name={year}
+        className={className}
+        key={`select-${year}`}
+        doClick={() => isSelected || updateYear(year)}
+      />
+    )
+  })
+
+  const months = allMonths.map(month => {
+    const yearOnly = selected.year === 'All' || selected.month == 'All'
+    const isSelected = selected.month === month
+    const isEmpty = false
+    const className = [
+      'monthBtn',
+      isSelected && 'selected',
+      isEmpty && 'empty',
+      month.toLowerCase()
+    ]
+      .filter(c => c)
+      .join(' ')
+    return (
+      <SelectButton
+        name={month}
+        className={className}
+        key={`select-${month}`}
+        doClick={() => isSelected || updateMonth(month)}
+      />
+    )
+  })
+
   return (
     <section className={`${className} postHome`}>
-      {/*<SummaryList doClick={() => changeSection(0)} isOpen={openSection < 0} posts={recentPosts} title="Recent" key={'recent-posts'}/>)*/}
       {/*{listsByYear}*/}
       <ul className="yearSelector">
-        <li className={'selectWide'}>
-          <div>
-            <header>all</header>
-          </div>
-        </li>
-
-        <li>
-          <div>
-            <header>1</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>2</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>3</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>4</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>5</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>6</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>7</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>8</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>9</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>10</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>11</header>
-          </div>
-        </li>
-        <li>
-          <div>
-            <header>12</header>
-          </div>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <div><header>Recent</header></div>
-        </li>
-        <li>
-          <div><header>2020</header></div>
-        </li>
-        <li>
-          <div><header>2019</header></div>
-        </li>
-        <li>
-          <div><header>2018</header></div>
-        </li>
+        {years}
+        {months}
       </ul>
       {children}
-
+      {/*<SummaryList doClick={() => changeSection(0)} isOpen={openSection < 0} posts={recentPosts} title="Recent" key={'recent-posts'}/>)*/}
     </section>
   )
 }
 
-const PostNav = ({}) => {
-
-}
-
+const PostNav = ({}) => {}
 
 PostHome.defaultProps = {
-  className: '',
+  className: ''
 }
 
 export default PostHome
