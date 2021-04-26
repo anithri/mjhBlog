121815e5d14c41d0dbc5800e9666c18cf7a11696
Scott M Parrish <anithri@gmail.com>
2021-04-26 14:06:51 -0600

    layout work

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import cx from 'classnames'
 import * as styles from './styles.module.css'
 import { useSiteMetadata } from 'data'
+import {Link} from 'gatsby'
 
 export const Header = ({ className }) => {
   const metadata = useSiteMetadata()
   return (
     <header className={cx(className, styles.header)}>
-      <h1>{metadata.title}</h1>
+      <Link to={"/"}><h1>{metadata.title}</h1></Link>
     </header>
   )
 }
